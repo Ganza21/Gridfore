e70282ba4463e76306808a37f32b61f2a15c3de2
AndreyK <kozyrev.andrey.1993@gmail.com>
2017-10-15 01:29:13 +0300

    Created model to parse from file

---
+++ src/main/java/model/Model.java
@@ -0,0 +1,32 @@
+package model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Created by Андрей on 15.10.2017.
+ */
+@Getter
+@Builder
+public class Model {
+
+    private final Integer number;
+    private final Date date;
+    private final Integer tranz;
+    private final Integer volume;
+    private final String calledNum;
+    private final BigDecimal charge;
+
+    public Model(Integer number, Date date, Integer tranz, Integer volume, String calledNum, BigDecimal charge) {
+        this.number = number;
+        this.date = date;
+        this.tranz = tranz;
+        this.volume = volume;
+        this.calledNum = calledNum;
+        this.charge = charge;
+    }
+
+}
---
+++ src/main/java/parser/ParserFromFile.java
@@ -0,0 +1,7 @@
+package parser;
+
+/**
+ * Created by Андрей on 15.10.2017.
+ */
+public class ParserFromFile {
+}
