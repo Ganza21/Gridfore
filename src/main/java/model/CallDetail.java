c3d8e06ab01e81a6297777274c924cb194bfa44d
AndreyK <kozyrev.andrey.1993@gmail.com>
2017-10-18 01:27:33 +0300

    Add test to ParserData

--- src/main/java/model/CallDetail.java
+++ src/main/java/model/CallDetail.java
@@ -1,6 +1,8 @@
 package model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Setter;
 import org.apache.ignite.cache.query.annotations.QuerySqlField;
 
 import java.io.Serializable;
@@ -14,6 +16,8 @@ import java.util.List;
  * Created by Андрей on 15.10.2017.
  */
 @Getter
+@Setter
+@EqualsAndHashCode(of = {"number", "date", "tranz", "volume", "calledNum", "charge"})
 public class CallDetail implements Serializable{
     @QuerySqlField
     private String number;
@@ -29,6 +33,9 @@ public class CallDetail implements Serializable{
     private BigDecimal charge;
 
     public static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+
+    public CallDetail() {
+    }
 
     /**
      * Constructor to convert a List to an object
@@ -48,5 +55,4 @@ public class CallDetail implements Serializable{
             System.out.println("Запись кривая");
         }
     }
-
 }
--- src/test/java/ParserDataTest.java
+++ src/test/java/ParserDataTest.java
@@ -1,9 +1,45 @@
+import model.CallDetail;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import parser.ParserData;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static model.CallDetail.FORMAT;
+
 /**
  * Created by Андрей on 16.10.2017.
  */
 public class ParserDataTest {
 
-    public void testParserData(){
+    CallDetail callDetail = new CallDetail();
+    List<String> testString;
 
-    }
+        @Before
+        public void prepare(){
+
+            callDetail.setNumber("5165513961");
+            callDetail.setDate(LocalDate.parse("2017/04/02", FORMAT));
+            callDetail.setTranz(3);
+            callDetail.setVolume(105);
+            callDetail.setCalledNum("5167278881");
+            callDetail.setCharge(BigDecimal.valueOf(188.65));
+
+            testString = new ArrayList<>();
+            testString.add("5165513961,2017/04/02,3,105,5167278881,188.65");
+
+        }
+
+        @Test
+        public void testParseData() throws IOException {
+           Assert.assertTrue(callDetail.equals(ParserData.splitDataToObject(testString).get(0)));
+           Assert.assertEquals(callDetail, ParserData.splitDataToObject(testString).get(0));
+        }
 }
+
+
