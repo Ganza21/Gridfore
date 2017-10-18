fa3d5a89451c4a98204f7379f8686c6cf1b9aca3
AndreyK <kozyrev.andrey.1993@gmail.com>
2017-10-18 16:47:23 +0300

    fix ParseFromFiles: now reads data from all files in the directory

--- src/main/java/parser/ParserFromFile.java
+++ src/main/java/parser/ParserFromFile.java
@@ -2,7 +2,11 @@ package parser;
 
 import java.io.*;
 import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by Андрей on 15.10.2017.
@@ -10,16 +14,25 @@ import java.util.List;
 public interface ParserFromFile {
 
     /**
-     * The method reads data from a file
+     * The method reads data from a files
      * @param pathToFile
      * @return List<String> dataSet
      * @throws IOException
      */
     static List<String> getData(String pathToFile) throws IOException {
 
-        Path path = Paths.get(pathToFile);
-        List<String> dataSet = Files.readAllLines(path);
+        List<String> dataSet = new ArrayList<>();
 
+        List<Path> fileList = Files.walk(Paths.get(pathToFile))
+                .collect(Collectors.toList());
+
+            fileList.forEach(path -> {
+                try {
+                    dataSet.addAll(Files.readAllLines(path));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    }
+            });
         return dataSet;
     }
 }
